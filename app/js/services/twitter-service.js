ecd69cd03057a2f09cc80ed171043cbba2537a52
Geordy Molenaar <geordy@competa.com>
2014-12-29 16:04:08 +0100

    Initial commit Twitter factory

---
+++ app/js/services/twitter-service.js
@@ -0,0 +1,9 @@
+/**
+ * @ngdoc service
+ * @name TwitterService
+ * @description
+ * # Service to retrieve twitter messages
+ */
+smdc.factory('TwitterService',['$resource', 'restAPI', function($resource, restAPI) {
+	
+}]);
