04357c2264a007162ea9d1bc1c8d797339073268
Geordy Molenaar <geordy@competa.com>
2014-12-30 11:09:36 +0100

    Initial commit Twitter factory

---
+++ app/js/controllers/twitter.controller.js
@@ -0,0 +1,10 @@
+/**
+ * @ngdoc function
+ * @name smdc.controller:TwitterCtrl
+ * @description
+ * Twitter controller
+ */
+
+smdc.controller('TwitterCtrl',['$scope', 'TwitterService', function($scope, TwitterService) {
+	
+}]);
