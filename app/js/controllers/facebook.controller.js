17f022af67b2787c3b564489959d70a09003606e
Geordy Molenaar <geordy@competa.com>
2014-12-31 11:06:48 +0100

    Uses moment library to convert created_at

--- app/js/controllers/facebook.controller.js
+++ app/js/controllers/facebook.controller.js
@@ -19,7 +19,7 @@ smdc.controller('FacebookCtrl',['$scope', 'FacebookService', function($scope, Fa
 	FacebookService.posts.get(function(posts) {
 		// $scope.posts = posts.data;
 		angular.forEach(posts.data, function(posts) {
-			posts.created_time = parseFacebookDate(posts.created_time);
+			posts.created_time = moment(posts.created_time).fromNow();
 			$scope.posts.push(posts);
 		});
 	});
@@ -31,28 +31,4 @@ smdc.controller('FacebookCtrl',['$scope', 'FacebookService', function($scope, Fa
 		$scope.insights = insights;
 	});
 	
-	/**
-	 * Converts Facebook created_time to a readable date
-	 * @param  {string} time
-	 * @return {string}
-	 */
-	var parseFacebookDate = function(time){
-		var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
-		    diff = (((new Date()).getTime() - date.getTime()) / 1000),
-		    day_diff = Math.floor(diff / 86400);
-		
-		if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
-		    return;
-
-		return day_diff == 0 && (
-		        diff < 60 && "just now" ||
-		        diff < 120 && "1 minute ago" ||
-		        diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-		        diff < 7200 && "1 hour ago" ||
-		        diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-		    day_diff == 1 && "Yesterday" ||
-		    day_diff < 7 && day_diff + " days ago" ||
-		    day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
-	}
-
 }]);
