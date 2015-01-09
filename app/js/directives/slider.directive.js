bce7e995f0911eb4a6433a2bc294e53733a34e5d
Geordy Molenaar <geordy@competa.com>
2015-01-09 15:27:01 +0100

    Refactored slider directive

--- app/js/directives/slider.directive.js
+++ app/js/directives/slider.directive.js
@@ -1,6 +1,33 @@
-smdc.directive('slider', function() {
+smdc.directive('slider',['$document', function($document) {
 	return {
 		restrict: 'E',
-		templateUrl: 'views/partials/slider.html',
+		link: function(scope, e, a) {
+
+			var index = 0;
+			var currentOffset;
+
+			var ul = e.children();
+				ul = ul.children();
+				ul = ul[0];
+
+			var li = e.children();
+				li = li.children();
+				li = li[0].children;
+
+			function slide() {
+			    if(index >= li.length -1) {
+			        index = 0;  
+			        ul.style.left = 0;
+			    } else {
+			        index++;
+			        currentOffset = 500 * index;
+			        ul.style.left = '-' + currentOffset + 'px';			        
+			    }
+			}		
+			window.setInterval(slide, 2000);
+		},
+		templateUrl: function(e,a) {
+           return 'views/partials/' + a.data + '-slider.html'
+       }
 	}
-});
+}]);
